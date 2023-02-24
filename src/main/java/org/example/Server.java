94d24f1c53ed7f96350452c0e85be0f00c05caab
Jaskaran Toor <jaskarantoor00@gmail.com>
2023-02-24 09:59:28 -0800

    Added UI_Manager, UI, and Server classes.

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Server {
+
+    private Server Server;
+
+    private UI_Manager uiManager;
+
+    private Sprite_Manager sprite_manager;
+
+    private Path_Manager pathManager;
+
+    private Server();
+
+    public void getInstance(){
+
+    }
+
+    public void connectManagers(){
+
+    }
+}
