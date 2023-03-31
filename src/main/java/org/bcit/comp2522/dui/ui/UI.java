b0fab81862449e91ad336de2eb53a7704272beae
Pavanpreet Brar <pbrar73@my.bcit.ca>
2023-03-31 07:39:09 -0700

    Leaderboard is 80% completed.

--- src/main/java/org/bcit/comp2522/dui/ui/UI.java
+++ src/main/java/org/bcit/comp2522/dui/ui/UI.java
@@ -110,9 +110,9 @@ public class UI implements Drawable {
             case 4:
                 menu.carSelection();
                 break;
-//            case 5:
-//                menu.gameOver();
-//                break;
+            case 5:
+                menu.Leaderboard();
+                break;
 //            case 6:
 //                menu.gameOver();
 //                break;
